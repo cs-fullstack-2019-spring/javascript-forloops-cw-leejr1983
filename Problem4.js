a191a53a7a6165afdc18ebb6041ec92cec2a2603
leejr1983 <leejr1983@yahoo.com>
2019-01-14 04:24:46 -0600

    classwork

---
+++ Problem4.js
@@ -0,0 +1,11 @@
+// Exercise 4:
+// Use a for loop to print from 0 to 50. Once it gets to 10, print "TEN!!!" instead of the number.
+
+for (i = 0; i<= 50; i++){
+
+    if (i == 10)
+        console.log ("TEN!!!")
+
+    else console.log (i)
+
+}
