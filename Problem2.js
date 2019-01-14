e016e95fedcc21a5c6e10ba32ee69adef31100b2
leejr1983 <leejr1983@yahoo.com>
2019-01-14 03:53:40 -0600

    classwork

---
+++ Problem2.js
@@ -0,0 +1,11 @@
+// Exercise 2:
+// // Use a for loop to count from -5 to 5 three times in a row.
+
+for (var i = -5; i <=5; i++) {
+    console.log(i)}
+
+for (var i = -5; i <=5; i++) {
+    console.log(i)}
+
+for (var i = -5; i <=5; i++) {
+    console.log(i)}
