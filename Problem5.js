adfbad1d120b6390597dc8145fce4202964f9d67
leejr1983 <leejr1983@yahoo.com>
2019-01-14 05:13:38 -0600

    classwork

---
+++ Problem5.js
@@ -0,0 +1,10 @@
+// Exercise 5:
+// Prompt the user to enter a number. Prompt the user to enter another number.
+// Create a for loop that counts from the first number to the second number.
+
+number = prompt("Please enter a number.")
+number2 = prompt ("Please enter another number.")
+
+for (var i = number; i <= number2; i++){
+    console.log(i)
+}
